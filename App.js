cc40f47ee0af57ca48d4fb362fc191329bb1eb0c
Lucas de Lima Landim <127635545+LucasLandimLLL@users.noreply.github.com>
2023-11-12 21:18:45 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Chat from "./screens/Chat";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup"
+import Home from "./screens/Home"
+
+const Stack = createStackNavigator();
+
+function ChatStack () {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function RootNavigator () {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  )
+}
+
+export default function App() {
+  return <RootNavigator />
+}
---
+++ screens/Chat.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+
+export default function chat() {
+    return (
+    <GiftedChat />
+    )
+}
