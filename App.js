8d4466f8a6fc987b33836d3c95057a91b62cddb7
fonsecasBruno <brunofsouza669@gmail.com>
2023-11-18 19:54:09 -0300

    Navigation and Chat

--- App.js
+++ App.js
@@ -12,22 +12,66 @@ import Home from "./screens/Home"
 const Stack = createStackNavigator();
 const AuthenticatedUserContext = createContext({});
 
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthenticatedUserContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
+
 function ChatStack () {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Chat" component={Chat} />
+    <Stack.Navigator defaultScreenOptions={Home}>
+      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Chat" component={Chat}/>
     </Stack.Navigator>
   )
 }
 
+function AuthStack () {
+  return (
+  <Stack.Navigator defaultScreenOptions={Login} screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Login" component={Login}/>
+    <Stack.Screen name="Signup" component={Signup}/>
+  </Stack.Navigator>
+  )
+}
+
 function RootNavigator () {
+  const {user, setUser} = useContext(AuthenticatedUserContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setLoading(false);
+      }
+    );
+      return () => unsubscribe();
+  }, [user]);
+  if(loading) {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size={"large"}/>
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
+      { user ? <ChatStack/> : <AuthStack/>}
       <ChatStack />
     </NavigationContainer>
   )
 }
 
 export default function App() {
-  return <RootNavigator />
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  )
 }
--- screens/Chat.js
+++ screens/Chat.js
@@ -1,8 +1,77 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect, useCallback} from "react";
 import { GiftedChat } from "react-native-gifted-chat";
+import { TouchableOpacity, Text } from "react-native";
+import { collection, addDoc, orderBy, query, onSnapshot } from 'firebase/firestore';
+import { signOut } from "firebase/auth";
+import { auth, database } from "../config/firebase";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from '@expo/vector-icons'
+import cores from "../cores";
 
-export default function chat() {
+
+export default function Chat() {
+    const [messages, setMessages] = useState([]);
+    const navigation = useNavigation();
+
+    const onSignOut = () => {
+        signOut(auth).catch(error => console.log(error));
+    };
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity style={{
+                    marginRight: 10
+                }}
+                onPress={onSignOut}
+                >
+                    <AntDesign name="logout" size={24} color={cores.gray} style={{marginRight: 10}}/>
+                </TouchableOpacity>
+            )
+        });
+    }, [navigation]);
+
+    useLayoutEffect(() => {
+        const collectionRef = collection(database, 'chats');
+        const q = query(collectionRef, orderBy('createdAt', 'desc'));
+        
+        const unsubscribe = onSnapshot(q, snapshot => {
+            console.log('snapshot');
+            setMessages(
+                snapshot.docs.map(doc => ({
+                    _id: doc.id,
+                    createdAt: doc.data().createdAt,
+                    text: doc.data().text,
+                    user: doc.data().user
+                }))
+            )
+        });
+        return () => unsubscribe();
+    }, []);
+
+    const onSend = useCallback(( messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+
+        const { _id, createdAt, text, user } = messages[0];
+        addDoc(collection(database, 'chats'), {
+            _id,
+            createdAt,
+            text,
+            user
+        });
+    }, []);
+
     return (
-    <GiftedChat />
+    <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+            _id: auth?.currentUser?.email,
+            avatar: 'https://i.pravatar.cc/300'
+        }}
+        messagesContainerStyle={{
+            backgroundColor: '#fff'
+        }}
+    />
     )
 }
